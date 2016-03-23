436e8635bad8269ef8225ba0c7e8c9e012b09737
shuaikun <2440312922@qq.com>
2016-03-23 15:49:16 +0800

    Initial Commit

---
+++ CoreAnmationAndCore/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreAnmationAndCore
+//
+//  Created by caoshuaikun on 16/3/23.
+//  Copyright © 2016年 wuxiwenyan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CoreAnmationAndCore/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CoreAnmationAndCore
+//
+//  Created by caoshuaikun on 16/3/23.
+//  Copyright © 2016年 wuxiwenyan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
